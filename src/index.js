2e5bb34283a1b6206c68d284c604e940f1fed4fe
dialite <raymondolisa01@gmail.com>
2022-12-08 10:58:52 +0100

    Restructure components to have unique pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Calculator from './pages/calculator';
+import NavLink from './components/nav';
+import './modules-css/calculator.css';
+import './modules-css/nav.css';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="header">
+          <NavLink />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
