9b73071e29fb48b2924832425660dc830acd8285
dialite <raymondolisa01@gmail.com>
2022-12-08 06:57:34 +0100

    Add and modify quotes page

---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="container">
+    <h3>
+      Mathematics is not about numbers, equations, computations,
+      or algothrims: it is about understanding. - William Paul Thurston
+    </h3>
+  </div>
+);
+
+export default Quote;
