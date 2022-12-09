7cc0313393f55e69a0e9d02d53c65292b92ec615
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-12-09 09:29:12 -0800

    some changes

--- src/__tests__/home.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from 'react-test-renderer';
-import Home from '../components/Home';
-
-test('render Home', () => {
-  const renderHome = renderer.create(<Home />).toJSON();
-  expect(renderHome).toMatchSnapshot();
-});
--- src/__tests__/quote.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from 'react-test-renderer';
-import Quote from '../components/Quote';
-
-test('Render Quote', () => {
-  const renderQuote = renderer.create(<Quote />).toJSON();
-  expect(renderQuote).toMatchSnapshot();
-});
